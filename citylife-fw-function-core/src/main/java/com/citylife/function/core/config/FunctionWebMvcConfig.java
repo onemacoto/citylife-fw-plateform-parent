9e44728120c955a744032a80d94f8b4660eabb35
AzureAD\MingLiangChen <cmliang@cn.ibm.com>
2018-12-14 11:21:35 +0800

    update

--- citylife-fw-function-core/src/main/java/com/citylife/function/core/config/FunctionWebMvcConfig.java
+++ citylife-fw-function-core/src/main/java/com/citylife/function/core/config/FunctionWebMvcConfig.java
@@ -3,26 +3,23 @@ package com.citylife.function.core.config;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.alibaba.fastjson.support.springfox.SwaggerJsonSerializer;
-import com.citylife.function.core.method.support.UserArgumentResolver;
 
 import springfox.documentation.spring.web.json.Json;
 
 @Configuration
 public class FunctionWebMvcConfig implements WebMvcConfigurer {
 
-  @Autowired
-  private UserArgumentResolver userArgumentResolver;
+//  @Autowired
+//  private UserArgumentResolver userArgumentResolver;
 
   /**
    * 利用fastjson替换掉jackson，且解决中文乱码问题
@@ -46,9 +43,9 @@ public class FunctionWebMvcConfig implements WebMvcConfigurer {
 
   }
 
-  @Override
-  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-    resolvers.add(userArgumentResolver);
-  }
+//  @Override
+//  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+//    resolvers.add(userArgumentResolver);
+//  }
 
 }
