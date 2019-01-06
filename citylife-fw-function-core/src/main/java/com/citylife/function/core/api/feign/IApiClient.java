7a5c08d21a52fcf4f55d43ec7e1978c761cad501
citylife-master <onemacoto@outlook.com>
2019-01-06 23:03:22 +0800

    update

--- citylife-fw-function-core/src/main/java/com/citylife/function/core/api/feign/IApiClient.java
+++ citylife-fw-function-core/src/main/java/com/citylife/function/core/api/feign/IApiClient.java
@@ -1,7 +1,7 @@
 package com.citylife.function.core.api.feign;
 
 public interface IApiClient {
-  public static final String HEADER_TOKEN_KEY = "User-Token";
+  public static final String HEADER_TOKEN_KEY = "X-User-Token";
   public static final String VERSION_KEY = "version";
 
 }
