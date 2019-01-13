929afdfab1a53e3b3983ae0221aee9aba26aee24
citylife-master <onemacoto@outlook.com>
2019-01-13 21:54:02 +0800

    init

--- citylife-fw-common-basis/src/main/java/com/citylife/common/constants/SystemMessageConsts.java
+++ citylife-fw-common-basis/src/main/java/com/citylife/common/constants/SystemMessageConsts.java
@@ -3,4 +3,5 @@ package com.citylife.common.constants;
 public class SystemMessageConsts {
   public static final String SYSTEM_ERROR_UNEXPECTED = "system.error.unexpected";
   public static final String SYSTEM_ERROR_FALLBACK = "system.error.fallback";
+  public static final String SYSTEM_ERROR_OAuth2 = "system.error.oauth";
 }
